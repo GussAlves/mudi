303cff02c336392ac1263a5c234b6c99009b4858
gusal <gus.alves.araujo@gmail.com>
2021-08-03 23:33:12 -0300

    /home url setting | fixing view bugs

--- src/main/java/com/gussalves/mvc/mudi/controller/HomeController.java
+++ src/main/java/com/gussalves/mvc/mudi/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.gussalves.mvc.mudi.repository.ProductOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,12 @@ public class HomeController {
     public String forStatus(@PathVariable("status") String status, Model model) {
         Iterable<ProductOrder> productOrders = productOrderRepository.findByStatus(ProductStatus.valueOf(status.toUpperCase(Locale.ROOT)));
         model.addAttribute("orders", productOrders);
+        model.addAttribute("status", status);
         return "home";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError() {
+        return "redirect:/home";
     }
 }
